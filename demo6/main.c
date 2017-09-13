2ae69a13b06d741ef255b025d85d3755e2aca3cd
Qinka <qinka@live.com>
2017-09-13 15:13:52 +0800

    update;

--- demo6/main.c
+++ demo6/main.c
@@ -1,4 +1,5 @@
 #include "stm32f10x.h"
+#include "misc.h"
 
 int _exit(void)
 {
@@ -12,27 +13,29 @@ int main(void)
   // serial for baud rate 115200
   // configure GPIOA's port(used by usart, and PA0)
   RCC -> APB2ENR |= 0b1 << 2;
-  RCC -> APB2ENR |= 0b1 << 3;
-  GPIOB -> CRL &= ~(0xF << 5);
-  GPIOB -> CRL |= 0b11 << 5;
-  GPIOB -> ODR |= 0b1 << 5;
-  // enable USART1's clock
   RCC -> APB2ENR |= 0b1 << 14;
-  // configure TIM5 clock
-  RCC -> APB1ENR |= 0b1 << 3;
+  
   // setting GPIOA IO
   GPIOA -> CRH &= 0XFFFFF00F;
   GPIOA -> CRH |= 0X000008B0;
   // reset USART1
   RCC -> APB2RSTR |=  0b1 << 14;
   RCC -> APB2RSTR &= ~(0b1 << 14);
+  // enable USART1's clock
+  RCC -> APB2ENR |= 0b1 << 14;
   // config usart1
   USART1 -> BRR = 0x0271; // for mantissa 0x27; faction 0x01;
   USART1 -> CR1 |= 0x200C; // 1 bit for stop, without parity
+  
+
+  
+  // configure TIM5 clock
+  RCC -> APB1ENR |= 0b1 << 3;
   // setting PA0
   GPIOA -> CRL &= ~(0xF << 0);
   GPIOA -> CRL |= 0b1000 << 0;
-  GPIOA -> ODR &= ~(0b1 << 0); // pull down
+  //GPIOA -> ODR &= ~(0b1 << 0); // pull down
+  //GPIOA -> BRR |= (0b1 << 0);
   // setting TIM5
   TIM5 -> ARR = 0xFFFF;
   TIM5 -> ARR = 72 - 1;
@@ -45,13 +48,12 @@ int main(void)
   TIM5 -> CR1 |= 0b1 << 0;
   // init interrupt for TIM5
   // nvic (group 2, p (2,2))
-  u32 tmp = SCB -> AIRCR;
-  tmp &= 0xF8FF;
-  tmp |= 0x05FA << 16;
-  tmp |= 0x0200; // ((~ 2) & 0b111) << 8
-  SCB -> AIRCR = tmp;
-  NVIC -> ISER[TIM5_IRQn /32] |= (0b1 << TIM5_IRQn % 32);
-  NVIC -> IP[TIM5_IRQn] |= 0b1010; // (pp << (4 - g) | ((rp & (0xf >> g))& 0xf
+  NVIC_InitTypeDef NVIC_InitStructure;
+  NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);  
 
   
   GPIOB -> ODR &= ~(0b1 << 5);
@@ -68,7 +70,8 @@ int main(void)
 
 void sendCharCom(u8 chr)
 {
-  USART1 -> DR = chr;  
+  while((USART1 -> SR & 0x40) == 0);
+  USART1 -> DR = chr;
 }
 
 void TIM5_IRQHandler(void)
@@ -92,6 +95,7 @@ void TIM5_IRQHandler(void)
       goto reset;
     }
   }
+  goto next;
   return;
   // send time via usart and reset TIM5
  reset:
