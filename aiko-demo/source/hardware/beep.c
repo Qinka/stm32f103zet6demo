35f6e0c684e12c0f380b8cafe14f7b94ab782b80
Qinka <qinka@live.com>
2017-10-14 00:18:44 +0800

    fix;update

--- aiko-demo/source/hardware/beep.c
+++ aiko-demo/source/hardware/beep.c
@@ -4,6 +4,7 @@
 #include <firmware/stm32f10x.h>
 #include <firmware/stm32f10x_gpio.h>
 #include <firmware/stm32f10x_rcc.h>
+#include <hardware/beep.h>
 
  void beep_config(void) {
    // initialize clock
@@ -13,11 +14,12 @@
    BEEP_GPIO.GPIO_Mode  = GPIO_Mode_Out_PP;
    BEEP_GPIO.GPIO_Speed = GPIO_Speed_50MHz;
    GPIO_Init(GPIOC,& BEEP_GPIO);
-   GPIO_SetBits(GPIOC,GPIO_Pin_0);
- }
- void beep_on(void) {
-  GPIO_ResetBits(GPIOC,GPIO_Pin_0);
+   beep_on();
+   for(int i = 0xFFFF; i>0 ;--i);
  }
  void beep_off(void) {
+  GPIO_ResetBits(GPIOC,GPIO_Pin_0);
+ }
+ void beep_on(void) {
   GPIO_SetBits(GPIOC,GPIO_Pin_0);
  }
